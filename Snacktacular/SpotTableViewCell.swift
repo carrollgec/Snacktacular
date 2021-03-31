e30ba82e36e2a672e926d8d39bb4e79c45e5c8c1
Grace <carrolhn@bc.edu>
2021-03-31 18:56:28 -0400

    Completed 8.3

---
+++ Snacktacular/SpotTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  SpotTableViewCell.swift
+//  Snacktacular
+//
+//  Created by Grace Carroll on 3/31/21.
+//
+
+import UIKit
+
+class SpotTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
