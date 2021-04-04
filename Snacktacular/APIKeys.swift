f0fbe16b0de9d8ec98ba02a9c69b6de61454ff91
Grace <carrolhn@bc.edu>
2021-04-04 00:23:03 -0400

    Completed 8.7

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Grace Carroll on 4/4/21.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyDkJw1XoOwKFZIin-M5w0zDHfm7LNcWyRc"
+}
